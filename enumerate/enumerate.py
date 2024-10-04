ccd972abf35e7b00b97ee118e605422181199670
Praveena-Krishnan <praveenakrishnan2003@gmail.com>
2024-10-04 11:11:54 +0530

    enumerate commit

---
+++ enumerate/enumerate.py
@@ -0,0 +1,20 @@
+"""
+enumerate(iterable, start=0): Returns an enumerate object that produces tuples containing a count (from start, which defaults to 0) and the values obtained from iterating over the sequence.
+The enumerate function is commonly used to loop over a list and have an automatic counter/index along with the values.
+
+"""
+l1=["mango","python","dog"]
+print(list(enumerate(l1,100)))#returns the list with count starting from 100
+
+l2=enumerate(l1)
+print(next(l2)) #like iteration, it prints the next next element. 
+
+#reversed list enumerate function
+print(list(enumerate(reversed(l1))))#prints only the reverse of the 
+
+for index , value in enumerate(reversed(l1)):
+    print(len(l1)-index-1,value)#prints in reverse order of both index and value
+
+    
+    
+    
