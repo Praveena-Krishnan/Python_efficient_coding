10e98e38fa9f882168a2b40e672356961938a2f6
Praveena-Krishnan <praveenakrishnan2003@gmail.com>
2024-10-04 11:35:31 +0530

    "Added new function 'addition' and example usage of map() with it, and added list() conversion to existing map() example."

--- Map/mapfunc.py
+++ Map/mapfunc.py
@@ -1,8 +1,19 @@
 """map(function, iterable): Applies a given function to each item of an iterable (such as a list or tuple) and returns a map object.
 Returns a map object which is an iterator, so we can iterate over its elements."""
 
+def addition(n):
+    return n + n
+
+# We double all numbers using map()
+numbers = (1, 2, 3, 4)
+result = map(addition, numbers)
+print(list(result))
+
+
 print(map(lambda x: x**2, [1, 2, 3, 4]))#returns a map object
 print(list(map(lambda x: x**2, [1, 2, 3, 4])))
+
+
 
 
 
