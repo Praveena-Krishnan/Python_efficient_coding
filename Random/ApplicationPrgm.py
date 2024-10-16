b4667ca715072bb3aa2b90314eb9eb7000a4dde2
praveena-krishnan <praveenakrishnan2003@gmail.com>
2024-10-16 21:40:19 +0530

    Random choices application

---
+++ Random/ApplicationPrgm.py
@@ -0,0 +1,35 @@
+"""
+Application of using random choices in Python to generate random outcomes.
+Syntax : random.choices(sequence, weights=None, cum_weights=None, k=1) 
+
+"""
+import random
+
+class flashcard:
+    def __init__(self):
+      
+        self.fruits={'apple':'red',
+                     'orange':'orange',
+                     'watermelon':'green',
+                     'banana':'yellow'}
+        
+    def quiz(self):
+        while (True):
+          
+            fruit, color = random.choice(list(self.fruits.items()))
+            
+            print("What is the color of {}".format(fruit))
+            user_answer = input()
+            
+            if(user_answer.lower() == color):
+                print("Correct answer")
+            else:
+                print("Wrong answer")
+                
+            option = int(input("enter 0 , if you want to play again : "))
+            if (option):
+                break
+
+print("welcome to fruit quiz ")
+fc=flashcard()
+fc.quiz()
