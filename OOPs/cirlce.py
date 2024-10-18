d239b626dbf1a7dad4e2a68153b10a1c49b294f0
praveena-krishnan <praveenakrishnan2003@gmail.com>
2024-10-18 11:44:57 +0530

    oops

---
+++ OOPs/cirlce.py
@@ -0,0 +1,9 @@
+class circle:
+    def __init__(self,rad):
+        self.radius=rad
+        
+    def area(self):
+        return 3.14 * (self.radius ** 2)
+    
+my_circle=circle(5)
+print(my_circle.area())
